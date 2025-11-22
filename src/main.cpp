4b6e271f068f3b839326fc4f0904e92e9e60bad6
MoeLeak <i@leak.moe>
2025-11-22 20:33:16 +0800

    fix: Crash when exiting

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,10 @@
 int main(int argc, char *argv[]) {
   QGuiApplication app(argc, argv);
   QQuickStyle::setStyle("Material");
-  QQmlApplicationEngine engine;
 
   Backend backend;
+
+  QQmlApplicationEngine engine;
   engine.rootContext()->setContextProperty("backend", &backend);
 
   // To handle failure to load
